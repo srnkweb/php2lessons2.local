914341e5d36fb2b94617cefb7dbf96d8e6466733
srnk <srnkweb@gmail.com>
2025-09-30 12:28:25 +0300

    На данном этапе разработки реалезован ЧПУ ко всем существующим страницам.

--- public/App/templates/header/article.php
+++ public/App/templates/header/article.php
@@ -4,7 +4,7 @@
    class=" button-nav hoverNav">Домашнее задание</a>
 <a href="#"
    class=" button-nav active">Просмотор выбраной новости</a>
-<a href="http://php2lessons2.local/App/controllers/news.php"
+<a href="http://php2lessons2.local/news"
    class=" button-nav hoverNav">К новостям</a>
     </nav>
 </header>
--- public/App/templates/main/news.php
+++ public/App/templates/main/news.php
@@ -2,7 +2,7 @@
 <?php
     $data = array_reverse($data);
     foreach ($data as $key){ ?>
-          <h2><a href="http://php2lessons2.local/App/controllers/article.php?id=<?php echo $key->id ?>">
+          <h2><a href="http://php2lessons2.local/news/article/<?php echo $key->id ?>">
                   <?php echo $key->id .'. ' . $key->title ?></a></h2>
       <?php
     }
